1be8b5791ceb32dc9db7bf7af9c787e09e187651
Harry <hjperriton@gmail.com>
2025-10-26 21:35:45 +0000

    Fixed node import behaviour and relearned node server.

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-// server.js -
+// Server.js is going to be my main server file for my backend.
+// I need to do the following:
+/*
+1. Get the server running (Hello World!) - COMPLETE. 
+2. Get index.html to load.
+3. Get the CSS to load with the HTML.
+4. Get routes prepared for all the pages on my page.
+*/
+// This will be the perfect starting point for my code.
 
-// TUTORIALS FOR EXPRESS SUCK ASS WHY AM I STRUGGLING SO MUCH WITH BASIC SHIT.
+// Import express module.
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const app = express();
+// Declare instance of express as server. Declare port number globally.
+const server = express();
+const port = 3000;
 
-// Recreate __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+// Route for hello, world which is at default route '/'.
+server.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+// listen() takes a port number and then listens on that port.
+// The route at '/' is hello, world. So if you go to localhost:port you will go to hello, world by default.
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
